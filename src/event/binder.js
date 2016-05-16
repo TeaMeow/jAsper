b2df3bd4905a5094e0cb752b2653e72d6acc2460
Yami Odymel <yamiodymel@gmail.com>
2016-05-16 16:43:03 +0000

    Fixed binder when the binds undefined error.

--- src/event/binder.js
+++ src/event/binder.js
@@ -1,7 +1,7 @@
 jA.binder = function(binds, rebind)
 {
     rebind = rebind || false;
-    
+
     for(var i in binds)
     {
         /** Split the event and the target first */
@@ -10,13 +10,13 @@ jA.binder = function(binds, rebind)
             events  = splits[0],
         /** Split the targets */
             targets = splits[1].split('&');
-        
-        
+
+
         /** Detect what to bind based on different events */
         function bindThis(target, events, bind)
         {
             var event = events.split(' ');
-            
+
 
             switch(target)
             {
@@ -24,13 +24,13 @@ jA.binder = function(binds, rebind)
                 case ' Window':
                     target = window;
                     break;
-                
+
                 case ' document':
                 case ' Document':
                     target = document;
                     break;
             }
-            
+
 
             for(var i in event)
             {
@@ -41,31 +41,31 @@ jA.binder = function(binds, rebind)
                 {
                     if(rebind)
                         jA(target).off('scroll');
-                        
+
                     jA(target).scrollBottom(bind);
                 }
                 else if(e == 'ready')
                 {
                     if(rebind)
                         jA(target).off('DOMContentLoaded');
-                        
+
                     jA(target).ready(bind);
                 }
                 else if(e != '')
                 {
                     if(rebind)
                         jA(target).off(e);
-                        
+
                     jA(target).on(e, bind);
                 }
             }
         }
-        
-        
-        /** Each target */ 
+
+
+        /** Each target */
         for(var t in targets)
             /** Bind each callback if it's a callback array */
-            if(binds[i].isArray)
+            if(typeof binds[i] !== 'undefined' && binds[i].isArray)
                 for(var f in binds[i])
                     bindThis(targets[t], events, binds[i][f]);
             /** Or not, lol */
