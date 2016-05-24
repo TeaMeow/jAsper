d085f6ff7d4de3c9c6b1cda5aafaa609f4234a0e
Yami Odymel <yamiodymel@gmail.com>
2016-05-24 16:30:12 +0000

    Fixed that cannot generate the past years.

--- src/extend/gen-date.js
+++ src/extend/gen-date.js
@@ -2,7 +2,7 @@ jA.fn.genDate = function(type, direction)
 {
     type      = type      || 'day';
     direction = direction || 'past';
-    
+
     var now = new Date();
 
     return this.each(function(index, el)
@@ -10,14 +10,14 @@ jA.fn.genDate = function(type, direction)
         var year  = now.getFullYear(),
             month = 1,
             day   = 1;
-                
+
         switch(type)
         {
             case 'year':
                 for(var i = 0; i < 110; i++)
                 {
                     var option      = document.createElement('option');
-                        option.text = (direction == 'Past') ? year-- : year++;
+                        option.text = (direction == 'past') ? year-- : year++;
                     el.add(option);
                 }
                 break;
@@ -27,18 +27,18 @@ jA.fn.genDate = function(type, direction)
                 {
                     var option      = document.createElement('option');
                         option.text = (month < 10) ? '0' + month : month;
-                        
+
                     month++;
                     el.add(option);
                 }
                 break;
-             
+
             case 'day':
                 for(var i = 0; i < 31; i++)
                 {
                     var option      = document.createElement('option');
                         option.text = (day < 10) ? '0' + day : day;
-                        
+
                     day++;
                     el.add(option);
                 }
